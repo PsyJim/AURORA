f8b566fbe47062e4a024b04bbce414f6e4d7eee3
Jim Acosta <jimgerardo@outlook.com>
2020-12-01 12:21:09 -0700

    Ouch

--- setup.py
+++ setup.py
@@ -5,6 +5,12 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open("LICENSE", "r") as fl:
+    license = fl.read()
+
+with open("requirements.txt", "r") as fr:
+    requirements = fr.read()
+
 setuptools.setup(
     name="DCViewer-PsyJim",
     version="0.2.0",
@@ -13,8 +19,10 @@ setuptools.setup(
     description="A viewer for data cubes",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    license=license,
     url="https://github.com/PsyJim/DCViewer",
     packages=setuptools.find_packages(),
+    install_requires=[requirements],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
